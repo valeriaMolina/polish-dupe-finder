4c5ac151f471384aec063b3010a0e44cf443e5ff
Valeria Molina <valeriamolina123@gmail.com>
2024-11-11 18:39:36 -0500

    finished dupes window

--- polish-finder-app/src/components/DupesWindow.vue
+++ polish-finder-app/src/components/DupesWindow.vue
@@ -3,20 +3,50 @@
     <div class="px-3">
       <h1>Dupes</h1>
       <hr />
-      <div>
-        <p>Do you know a dupe? Add it here!</p>
+      <p>Do you know a dupe? Add it here!</p>
+      <div class="row row-cols-md-6">
+        <div v-for="polish in props.duplicates" :key="polish.polish_id">
+          <PolishCard
+            :id="polish.polish_id"
+            :polishName="polish.name"
+            :brandName="polish.brand.name"
+            :pictureUrl="polish.image_url"
+            :isLiked="likedPolishes.includes(polish.polish_id)"
+            @update:is-liked="updateLikeStatus()"
+          ></PolishCard>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { getUserLikes } from '@/apis/likesAPI'
+import PolishCard from './PolishCard.vue'
+import { useAuthStore } from '@/stores/auth'
+import { ref, onMounted } from 'vue'
+
+const authStore = useAuthStore()
+const likedPolishes = ref([])
+
 const props = defineProps({
   duplicates: {
     type: Array,
     required: true
   }
 })
+
+onMounted(async () => {
+  if (authStore.getIsLoggedIn) {
+    const likes = await getUserLikes(authStore.getEmail)
+    likedPolishes.value = likes.map((like) => like.polish_id)
+  }
+})
+
+const updateLikeStatus = async () => {
+  const likes = await getUserLikes(authStore.getEmail)
+  likedPolishes.value = likes.map((like) => like.polish_id)
+}
 </script>
 
 <style>
